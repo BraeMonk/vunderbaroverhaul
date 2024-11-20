d63b465bb7b67284fb29f6fdee59661bb74b1003
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2024-11-20 00:38:19 -0500

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,7 +1,7 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
     const playButton = document.getElementById('playBtn');
-    const video = document.getElementById('');
+    const video = document.getElementById('video');
 
     playButton.addEventListener('click', function() {
         video.play();
